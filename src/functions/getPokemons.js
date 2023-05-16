ec998f7f652b01fd6b359915f90d338dd28f2f48
Thiago Koch <tkoch.dev@gmail.com>
2023-05-16 20:57:52 -0300

    feat: add createContent function with loadAnimation

--- src/functions/getPokemons.js
+++ src/functions/getPokemons.js
@@ -3,11 +3,11 @@ import axios from "axios";
 export const getPokemons = (setPokemons) => {
 
   const endPoints = [];
-  for (var i = 1; i < 21; i++ ) {
+  for (var i = 1; i < 50; i++ ) {
     endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
   }
 
-  const endPointsResponses = axios.all(endPoints.map((endPoint) => axios.get(endPoint)))
+  axios.all(endPoints.map((endPoint) => axios.get(endPoint)))
   .then((resp) => setPokemons(resp))
   .catch((error) => console.log(error));
 };
