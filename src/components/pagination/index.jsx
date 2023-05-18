275319024e917cdb0e55f45c169ae48b978c2ff1
Thiago Koch <tkoch.dev@gmail.com>
2023-05-18 11:50:31 -0300

    fix: add toLowerCase in filter

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function BasicPagination({count, currentPage, onPageChange, pokemons}) {
+export default function BasicPagination({count, currentPage, onPageChange}) {
 
   return (
     <Stack spacing={2} style={{display:'flex',flexDirection:'row', justifyContent:'center', padding:'1.5rem 0 1.0rem 0'}}>
